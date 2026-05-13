6346628bd5efc843097b5a56205ee0e2d5ec58a3
pmnlla <39027165+pmnlla@users.noreply.github.com>
2026-05-13 18:18:25 -0400

    ahhh fuck this

--- sys/util/alterna.sh
+++ sys/util/alterna.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-setRndHostname() {
-    ID=$(hexdump -n 3 -e '"%06X\n"' /dev/urandom)
-    NAME="hyperdiver-$ID"
-    echo "Setting hostname to $NAME"
-    echo $NAME > /etc/hostname
+setup_opkg() {
+    echo "Copying /opt to /log/0/hd/opt for persistence..."
+    mkdir -p /log/0/hd/opt
+    cp -a /opt/* /log/0/hd/opt/
+    echo "Overriding /opt with a bind mount to /log/0/hd/opt..."
+    mount --bind /log/0/hd/opt /opt
+
+    wget https://bin.entware.net/armv7sf-k3.2/installer/generic.sh -O /tmp/entware-setup-generic.sh
+    chmod +x /tmp/entware-setup-generic.sh
+    /tmp/entware-setup-generic.sh
 }
 
-setRndHostname
+redo_opt() {
+    echo "Overriding /opt with a bind mount to /log/0/hd/opt..."
+    mount --bind /log/0/hd/opt /opt
+}
+
+if [ ! -f "/log/0/.firstboot_complete" ] && [ $1 = "init"]; then
+    setup_opkg
+    touch /log/0/.firstboot_complete
+    echo "Firstboot setup complete."
+else
+    redo_opt
+fi
